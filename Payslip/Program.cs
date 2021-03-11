20e3c9a1ad30a7b69546520ace4152db64dc3277
Meng Zhang <meng.zhang@myob.com>
2021-03-11 19:07:10 +1100

    Code refinement

--- Payslip/Program.cs
+++ Payslip/Program.cs
@@ -6,18 +6,18 @@ namespace Payslip
 {
     class Program
     {
-        string inputType;
-        string inputValue;
+        // string inputType;
+        // string inputValue;
         static void Main(string[] args)
         {
             PayDetails payDetails = new PayDetails();
-            userInput("name", payDetails);
-            userInput("annualSalary", payDetails);
-            userInput("superRate", payDetails);
-            userInput("pDate", payDetails);
-            userInput("print", payDetails);
+            UserInput("name", payDetails);
+            UserInput("annualSalary", payDetails);
+            UserInput("superRate", payDetails);
+            UserInput("pDate", payDetails);
+            UserInput("print", payDetails);
         }
-        static void userInput (string inputType, PayDetails payDetails){
+        static void UserInput (string inputType, PayDetails payDetails){
             Regex regexSalary = new Regex(@"^-?[0-9][0-9\.]+$"); //Regex to tolerate decimal point
             // Regex regexSuper = new Regex("^([1-9]\d?|100)$");
             // Regex regexDate = new Regex(@"^\d$");
@@ -27,7 +27,7 @@ namespace Payslip
                     string firstName = Console.ReadLine();
                     Console.WriteLine("Please input your surname:");
                     string surName = Console.ReadLine();
-                    payDetails.setName (firstName, surName);
+                    payDetails.SetName (firstName, surName); //Consider to decouple
                 }
                 catch(Exception e) {
                     Console.WriteLine("The name input is invalid" + "\n" + e.Message);
@@ -37,17 +37,15 @@ namespace Payslip
             else if (inputType == "annualSalary"){
                     Console.WriteLine("Please enter your annual salary:");
                     try {
-                        string inputSalary;
-                        double inputValue;
-                        inputSalary = Console.ReadLine();
+                        string inputSalary = Console.ReadLine();
                         while (regexSalary.IsMatch(inputSalary) == false){
                             Console.WriteLine("Wrong format, the annual salary should be numbers only, please re-enter your annual salary:");
                             inputSalary = Console.ReadLine();
                         }
-                        inputValue = Convert.ToDouble(inputSalary);
-                        payDetails.setGrossIncome (inputValue);
-                        payDetails.setIncomeTax (inputValue);
-                        payDetails.setNetIncome ();
+                        double inputValue = Convert.ToDouble(inputSalary);
+                        payDetails.SetGrossIncome (inputValue);
+                        payDetails.SetIncomeTax (inputValue);
+                        payDetails.SetNetIncome ();
                     }
                     catch (Exception e) {
                         throw e;
@@ -56,17 +54,15 @@ namespace Payslip
             else if (inputType == "superRate"){
                 Console.WriteLine("Please enter your super rate:");
                 try {
-                    string inputRate;
-                    double inputValue;
-                    inputRate = Console.ReadLine();
+                    string inputRate = Console.ReadLine();
                     while (regexSalary.IsMatch(inputRate) == false){
                         Console.WriteLine("Wrong format, the super rate should be numbers only, please re-enter your super rate:");
                         inputRate = Console.ReadLine();
                     }
-                    inputValue = Convert.ToDouble(inputRate);
-                    payDetails.setSuper(inputValue);
+                    double inputValue = Convert.ToDouble(inputRate);
+                    payDetails.SetSuper(inputValue);
                 }
-                    catch (Exception e) {
+                catch (Exception e) {
                         throw e;
                 }
             }
@@ -75,63 +71,64 @@ namespace Payslip
                 string pStartDate = Console.ReadLine();
                 Console.WriteLine("Please enter your payment end date:");
                 string pEndDate = Console.ReadLine();
-                payDetails.setPayPeriod (pStartDate, pEndDate);
+                payDetails.SetPayPeriod (pStartDate, pEndDate);
             }
             else if (inputType == "print") {
-                payDetails.printPayDetails();
+                payDetails.PrintPayDetails();
             }
         }
     }
     public class PayDetails {
-        private string name;
-        private string payPeriod;
-        private double grossIncome;
-        private double incomeTax;
-        private double netIncome;
-        private double super;
-        public static List<double> taxRate = new List<double> { 0.19, 0.325, 0.37, 0.45 };
+        private string _name;
+        private string _payPeriod;
+        private double _grossIncome;
+        private double _incomeTax;
+        private double _netIncome;
+        private double _super;
+        private static List<double> taxRate = new List<double> { 0.19, 0.325, 0.37, 0.45 };
         private static List<int> taxThreshold = new List<int> { 18200, 37000, 87000, 180000 };
-        public void setName (string name, string surName) {
-            this.name = name + " " + surName;
+        private static List<int> taxBracket = new List<int> {0, 3572, 19822, 54232};
+        public void SetName (string firstName, string surName) {
+            this._name = firstName + " " + surName;
         }
-        public void setPayPeriod (string startDate, string endDate){
-            this.payPeriod = startDate + " - " + endDate;
+        public void SetPayPeriod (string startDate, string endDate){
+            this._payPeriod = startDate + " - " + endDate;
         }
-        public void setGrossIncome (double annualSalary) {
-            this.grossIncome = (int)Math.Floor(annualSalary/12);
+        public void SetGrossIncome (double annualSalary) {
+            this._grossIncome = (int)Math.Floor(annualSalary/12);
         }
-        public void setIncomeTax (double annualSalary) {
+        public void SetIncomeTax (double annualSalary) {
             if (annualSalary <= taxThreshold[0]) { // When income <= 18200
-                this.incomeTax = 0;
+                this._incomeTax = taxBracket[0];
             }
             else if (annualSalary <= taxThreshold[1]) { // When 18201 < income <= 37000
-                this.incomeTax = (int)Math.Floor(((annualSalary - taxThreshold[0]) * taxRate[0])/12);
+                this._incomeTax = (int)Math.Ceiling(((annualSalary - taxThreshold[0]) * taxRate[0])/12);
             }
             else if (taxThreshold[1] < annualSalary && annualSalary <= taxThreshold[2]) { // When 37000 < income <= 87000
-                this.incomeTax = (int)Math.Floor(((annualSalary - taxThreshold [1]) * taxRate[1] + (taxThreshold [1] - taxThreshold[0]) * taxRate[0])/12);
+                this._incomeTax = (int)Math.Ceiling(((annualSalary - taxThreshold [1]) * taxRate[1] + taxBracket[1])/12);
             }
             else if (taxThreshold[2] < annualSalary && annualSalary <= taxThreshold[3]) { // When 87000 < income <= 180000
-                this.incomeTax = (int)Math.Floor(((annualSalary - taxThreshold [2]) * taxRate[2] + (taxThreshold [2] - taxThreshold [1]) * taxRate[1] + (taxThreshold [1] - taxThreshold[0]) * taxRate[0])/12);
+                this._incomeTax = (int)Math.Ceiling(((annualSalary - taxThreshold [2]) * taxRate[2] + taxBracket[1] + taxBracket[2])/12);
             }
-            else if (taxThreshold[3] < annualSalary && annualSalary <= taxThreshold[4]) { // When 180000 < income
-                this.incomeTax = (int)Math.Floor(((annualSalary - taxThreshold [3]) * taxRate[3] + (taxThreshold [3] - taxThreshold [2]) * taxRate[2] + (taxThreshold [2] - taxThreshold [1]) * taxRate[1] + (taxThreshold [1] - taxThreshold[0]) * taxRate[0])/12);
+            else if (taxThreshold[3] < annualSalary) { // When 180000 < income
+                this._incomeTax = (int)Math.Ceiling(((annualSalary - taxThreshold [3]) * taxRate[3] + taxBracket[1] + taxBracket[2] + taxBracket[3])/12);
             }
         }
-        public void setNetIncome () {
-            this.netIncome = grossIncome - incomeTax;
+        public void SetNetIncome () {
+            this._netIncome = _grossIncome - _incomeTax;
         }
-        public void setSuper (double superRate) {
-            this.super = grossIncome * (superRate/100);
+        public void SetSuper (double superRate) {
+            this._super = (int)Math.Floor(_grossIncome * (superRate/100));
         }
-        public void printPayDetails () {
+        public void PrintPayDetails () {
             Console.WriteLine(
                 "Your payslip has been generated:\n" +
-                "Name: " + name + "\n" +
-                "Pay Period: " + payPeriod + "\n" +
-                "Gross Income: " + grossIncome + "\n" +
-                "Income Tax: " + incomeTax + "\n" +
-                "Net Income: " + netIncome + "\n" +
-                "Super: " + super  + "\n" + "\n" +
+                "Name: " + _name + "\n" +
+                "Pay Period: " + _payPeriod + "\n" +
+                "Gross Income: " + _grossIncome + "\n" +
+                "Income Tax: " + _incomeTax + "\n" +
+                "Net Income: " + _netIncome + "\n" +
+                "Super: " + _super  + "\n" + "\n" +
                 "Thank you for using MYOB!"
                 );
         }
