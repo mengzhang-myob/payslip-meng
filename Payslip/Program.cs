be714e9bee8ed8b8e2b59794eae489d2176772f7
Meng Zhang <meng.zhang@myob.com>
2021-03-12 13:40:34 +1100

    Refactored with getter and setter

--- Payslip/Program.cs
+++ Payslip/Program.cs
@@ -43,9 +43,9 @@ namespace Payslip
                             inputSalary = Console.ReadLine();
                         }
                         double inputValue = Convert.ToDouble(inputSalary);
-                        payDetails.SetGrossIncome (inputValue);
-                        payDetails.SetIncomeTax (inputValue);
-                        payDetails.SetNetIncome ();
+                        payDetails.GrossIncome = payDetails.CalcGrossIncome(inputValue);
+                        payDetails.IncomeTax = payDetails.CalcIncomeTax(inputValue);
+                        payDetails.NetIncome = payDetails.CalcNetIncome();
                     }
                     catch (Exception e) {
                         throw e;
@@ -60,7 +60,7 @@ namespace Payslip
                         inputRate = Console.ReadLine();
                     }
                     double inputValue = Convert.ToDouble(inputRate);
-                    payDetails.SetSuper(inputValue);
+                    payDetails.Super = payDetails.CalcSuper(inputValue);
                 }
                 catch (Exception e) {
                         throw e;
@@ -79,52 +79,58 @@ namespace Payslip
         }
     }
     public class PayDetails {
-        private double _grossIncome;
-        private double _incomeTax;
-        private double _netIncome;
-        private double _super;
+        //Q regarding property: What to do with the calculation in the setter (no arguments allowed in the setter, how to parse taxRate/taxThreshold in)?
+        //My answers: Create independent function for calculation (not sure) 
         private static List<double> taxRate = new List<double> { 0.19, 0.325, 0.37, 0.45 };
         private static List<int> taxThreshold = new List<int> { 18200, 37000, 87000, 180000 };
         private static List<int> taxBracket = new List<int> {0, 3572, 19822, 54232};
         public string Name { get; set; }
 
         public string PayPeriod { get; set; }
+        
+        public int IncomeTax { get; set; }
 
-        public void SetGrossIncome (double annualSalary) {
-            this._grossIncome = (int)Math.Floor(annualSalary/12);
+        public int GrossIncome { get; set; }
+        public int NetIncome { get; set; }
+        public int Super { get; set; }
+
+        public int CalcGrossIncome (double annualSalary) {
+            return (int)Math.Floor(annualSalary/12);
         }
-        public void SetIncomeTax (double annualSalary) {
+        public int CalcIncomeTax (double annualSalary) {
             if (annualSalary <= taxThreshold[0]) { // When income <= 18200
-                this._incomeTax = taxBracket[0];
+                return taxBracket[0];
             }
             else if (annualSalary <= taxThreshold[1]) { // When 18201 < income <= 37000
-                this._incomeTax = (int)Math.Ceiling(((annualSalary - taxThreshold[0]) * taxRate[0])/12);
+                return (int)Math.Ceiling(((annualSalary - taxThreshold[0]) * taxRate[0])/12);
             }
             else if (taxThreshold[1] < annualSalary && annualSalary <= taxThreshold[2]) { // When 37000 < income <= 87000
-                this._incomeTax = (int)Math.Ceiling(((annualSalary - taxThreshold [1]) * taxRate[1] + taxBracket[1])/12);
+                return (int)Math.Ceiling(((annualSalary - taxThreshold [1]) * taxRate[1] + taxBracket[1])/12);
             }
             else if (taxThreshold[2] < annualSalary && annualSalary <= taxThreshold[3]) { // When 87000 < income <= 180000
-                this._incomeTax = (int)Math.Ceiling(((annualSalary - taxThreshold [2]) * taxRate[2] + taxBracket[1] + taxBracket[2])/12);
+                return (int)Math.Ceiling(((annualSalary - taxThreshold [2]) * taxRate[2] + taxBracket[1] + taxBracket[2])/12);
             }
             else if (taxThreshold[3] < annualSalary) { // When 180000 < income
-                this._incomeTax = (int)Math.Ceiling(((annualSalary - taxThreshold [3]) * taxRate[3] + taxBracket[1] + taxBracket[2] + taxBracket[3])/12);
+                return (int)Math.Ceiling(((annualSalary - taxThreshold [3]) * taxRate[3] + taxBracket[1] + taxBracket[2] + taxBracket[3])/12);
             }
+
+            return 0;
         }
-        public void SetNetIncome () {
-            this._netIncome = _grossIncome - _incomeTax;
+        public int CalcNetIncome () {
+            return GrossIncome - IncomeTax;
         }
-        public void SetSuper (double superRate) {
-            this._super = (int)Math.Floor(_grossIncome * (superRate/100));
+        public int CalcSuper (double superRate) {
+            return (int)Math.Floor(GrossIncome * (superRate/100));
         }
         public void PrintPayDetails () {
             Console.WriteLine(
                 "Your payslip has been generated:\n" +
                 "Name: " + Name + "\n" +
                 "Pay Period: " + PayPeriod + "\n" +
-                "Gross Income: " + _grossIncome + "\n" +
-                "Income Tax: " + _incomeTax + "\n" +
-                "Net Income: " + _netIncome + "\n" +
-                "Super: " + _super  + "\n" + "\n" +
+                "Gross Income: " + GrossIncome + "\n" +
+                "Income Tax: " + IncomeTax + "\n" +
+                "Net Income: " + NetIncome + "\n" +
+                "Super: " + Super  + "\n" + "\n" +
                 "Thank you for using MYOB!"
                 );
         }
